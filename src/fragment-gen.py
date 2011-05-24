51c2b61542545b2d93694c1d3595c4eee34e40eb
Tom Fiset <tmfset@gmail.com>
2011-05-23 22:06:48 -0400

    functional fragmenr-gen, and a test ly file

--- src/fragment-gen.py
+++ src/fragment-gen.py
@@ -1,11 +1,30 @@
-\version "2.8.1"
-\header {
-  tagline = ""
-}
-\paper{
-  ragged-right = ##t
-  indent = 0.0\mm
-  line-width = 100\pt
-}
+import os
+import subprocess
+import argparse
+import sys
 
-{ c'4 d'4 e'4 f'4 }
+parser = argparse.ArgumentParser(description='Generate lilypond fragments.')
+parser.add_argument('-r','--res',type=int,help='output resolution',default=200)
+parser.add_argument('infile',    help='name of lilypond file')
+parser.add_argument('name',      help='name of output file')
+
+args = parser.parse_args()
+print args
+
+# generate png
+r_lilypond = subprocess.call(['lilypond','--png',
+                                         '-dresolution=%i' % (args.res),
+                                         '-o%s' % (args.name),
+                                         args.infile])
+
+# if it exists, remove the postscript file
+name_ps = '%s.ps' % (args.name)
+if(os.path.exists(name_ps)) :
+  os.remove(name_ps)
+
+# if it exists, cut off the whitespace of the png file
+name_png = '%s.png' % (args.name)
+if(os.path.exists(name_png)) :
+  subprocess.call(['convert',name_png,'-trim','+repage',name_png])
+
+sys.exit(r_lilypond)
---
+++ src/test.ly
@@ -0,0 +1,12 @@
+\version "2.8.1"
+\header {
+  tagline = ""
+}
+\paper{
+  ragged-right = ##t
+  indent = 0.0\mm
+  line-width = 100\pt
+}
+
+{ c'4 d'4 e'4 f'4 }
+
